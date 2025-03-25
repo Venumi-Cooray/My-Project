4446933113a88551979c15ec8b677d4447198b28
Imandee Gunaratne <sandanithaimandeegunaratne@gmail.com>
2025-03-25 10:04:09 +0530

    Update program.py

--- program.py
+++ program.py
@@ -11,7 +11,7 @@ for car in cars:
     print(car.check_speed(90))
     print(car.check_speed(120))
     print(car.check_speed(160))
-    print(car.fuel_efficiency())
+    
     print()
 
     print(car.fuel_efficiency())
